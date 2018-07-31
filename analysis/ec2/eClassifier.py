df54dfcc503b6dc662e9d332ef259f199ebbb54f
Sunwoo Kang <swkang73@stanford.edu>
2018-07-31 12:52:29 -0700

    classifier kernel comparison

--- analysis/ec2/eClassifier.py
+++ analysis/ec2/eClassifier.py
@@ -6,13 +6,29 @@ reference: https://www.kaggle.com/cyberzhg/sklearn-pca-svm/data
 import numpy as np, matplotlib.pyplot as plt, seaborn as sns
 from matplotlib.colors import ListedColormap as LCmap
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+
 
 COMPONENT_NUM = 2
 RANOM_STATE = 0
 CIGTOTAL = 3
 G6, JUUL, BLU = 1, 2, 3
 h = 0.02 # for mesh grid  
+
+
+# helper function for rendering label
+def getlabel(index):
+	if index  % CIGTOTAL == 0:
+		return 'G6'
+	elif index % CIGTOTAL == 1: 
+		return 'Juul'
+	else: 
+		return 'Blu'
+
 
 # step 1: open files
 with open('training.csv', 'r') as reader:
@@ -32,8 +48,11 @@ pca = PCA(n_components=COMPONENT_NUM, whiten=True)
 pca.fit(train_data)
 train_data = pca.transform(train_data)
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=RANOM_STATE)
-clf.fit(train_data, train_label)
+kn_clf = KNeighborsClassifier().fit(train_data, train_label)
+nb_clf = GaussianNB().fit(train_data, train_label)
+lr_clf = LogisticRegression(solver='lbfgs').fit(train_data, train_label)
+mlp_clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=RANOM_STATE).fit(train_data, train_label)
+svm_clf = SVC(random_state=RANOM_STATE).fit(train_data, train_label)
 
 # step 3: plot PCAs into 2D plot
 # reference 1: https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html
@@ -67,26 +86,35 @@ y_min, y_max = train_data[:,1].min() - 1, train_data[:,1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=LCmap(('coral', 'cornflowerblue', 'olive')), alpha=0.8)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
+# title for the plots
+titles = ['K-Nearest Neighbour (KN)',
+	'Gaussian Naive Bayes (NB)',
+	'Logistic Regression (LR)',
+	'Single Vector Machine (SVM)',
+	'Artificial Neuron Network (ANN)']
 
-# Plot also the training points
-for idx, cl in enumerate(np.unique(train_label)):
-	plt.scatter(train_data[train_label == cl, 0], train_data[train_label == cl, 1],
-        c = LCmap(('red', 'blue', 'green'))(idx), label = getlabel(cl - 1))
+for i, clf in enumerate((kn_clf, nb_clf, lr_clf, svm_clf, mlp_clf)):
+	# Plot the decision boundary. For that, we will assign a color to each
+	# point in the mesh [x_min, m_max]x[y_min, y_max].
+	plt.subplot(2, 3, i + 1)
+	plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-plt.xlabel('1st Principal Component')
-plt.ylabel('2nd Principal Component')
-plt.legend(loc=1)
-plt.xticks(())
-plt.yticks(())
-plt.title('E-cig classification of PCA + ANN up to 20 trials')
+	print(len(np.c_[xx.ravel(), yy.ravel()]))
+	Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+	# Put the result into a color plot
+	Z = Z.reshape(xx.shape)
+	plt.contourf(xx, yy, Z, cmap=LCmap(('coral', 'cornflowerblue', 'olive')), alpha=0.8)
+	plt.scatter(train_data[:, 0], train_data[:, 1], c=train_label, cmap=LCmap(('red', 'blue', 'green')))
+
+	plt.xlabel('1st component')
+	plt.ylabel('2nd component')
+	plt.xlim(xx.min(), xx.max())
+	plt.ylim(yy.min(), yy.max())
+	plt.xticks(())
+	plt.yticks(())
+	plt.title(titles[i])
 
 plt.show()
-#fig.savefig('pca_%d_%d.png' % (i+1, j+1))
 
